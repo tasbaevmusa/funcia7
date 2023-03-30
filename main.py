0edb93cd120ba2b18511720323ba75c4eb59cd43
= <=>
2023-03-30 14:56:09 +0600

    ccc

---
+++ main.py
@@ -0,0 +1,14 @@
+n = int(input("Enter size of dictionary:"))
+vacation_schedule = {}
+for i in range(n):
+    name, day, month = input().split()
+    if month not in vacation_schedule:
+        vacation_schedule[month] = []
+    vacation_schedule[month].append(name)
+
+
+requested_month = input("Requested month:")
+if requested_month in vacation_schedule:
+    print(" ".join(vacation_schedule[requested_month]))
+else:
+    print()
